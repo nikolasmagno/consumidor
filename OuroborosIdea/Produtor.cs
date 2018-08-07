b6a5614a22154bdd186404577e3173e06f194ec0
Nikolas Magno Braga Leite - AeC <nikolas.leite@grupoaec.com.br>
2018-08-07 10:15:41 -0300

    Changing project name

--- OuroborosIdea/Produtor.cs
+++ OuroborosIdea/Produtor.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InitialIdea
+namespace Ouroboros.Sketch
 {
     public class Produtor
     {
--- OuroborosIdea/Program.cs
+++ OuroborosIdea/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace InitialIdea
+namespace Ouroboros.Sketch
 {
     class Program
     {
